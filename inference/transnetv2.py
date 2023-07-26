3d3a1dfa2b06881b81f2d6aaafe76a9f50982e01
hungsvdut2k2 <hungsvdut@gmail.com>
2023-07-26 08:50:04 +0700

    config input size

--- inference/transnetv2.py
+++ inference/transnetv2.py
@@ -4,26 +4,30 @@ import tensorflow as tf
 
 
 class TransNetV2:
-
-    def __init__(self, model_dir=None):
+    def __init__(self, input_size, model_dir=None):
         if model_dir is None:
             model_dir = os.path.join(os.path.dirname(__file__), "transnetv2-weights/")
             if not os.path.isdir(model_dir):
-                raise FileNotFoundError(f"[TransNetV2] ERROR: {model_dir} is not a directory.")
+                raise FileNotFoundError(
+                    f"[TransNetV2] ERROR: {model_dir} is not a directory."
+                )
             else:
                 print(f"[TransNetV2] Using weights from {model_dir}.")
 
-        self._input_size = (27, 48, 3)
+        self._input_size = input_size
         try:
             self._model = tf.saved_model.load(model_dir)
         except OSError as exc:
-            raise IOError(f"[TransNetV2] It seems that files in {model_dir} are corrupted or missing. "
-                          f"Re-download them manually and retry. For more info, see: "
-                          f"https://github.com/soCzech/TransNetV2/issues/1#issuecomment-647357796") from exc
+            raise IOError(
+                f"[TransNetV2] It seems that files in {model_dir} are corrupted or missing. "
+                f"Re-download them manually and retry. For more info, see: "
+                f"https://github.com/soCzech/TransNetV2/issues/1#issuecomment-647357796"
+            ) from exc
 
     def predict_raw(self, frames: np.ndarray):
-        assert len(frames.shape) == 5 and frames.shape[2:] == self._input_size, \
-            "[TransNetV2] Input shape must be [batch, frames, height, width, 3]."
+        assert (
+            len(frames.shape) == 5 and frames.shape[2:] == self._input_size
+        ), "[TransNetV2] Input shape must be [batch, frames, height, width, 3]."
         frames = tf.cast(frames, tf.float32)
 
         logits, dict_ = self._model(frames)
@@ -33,24 +37,30 @@ class TransNetV2:
         return single_frame_pred, all_frames_pred
 
     def predict_frames(self, frames: np.ndarray):
-        assert len(frames.shape) == 4 and frames.shape[1:] == self._input_size, \
-            "[TransNetV2] Input shape must be [frames, height, width, 3]."
+        assert (
+            len(frames.shape) == 4 and frames.shape[1:] == self._input_size
+        ), "[TransNetV2] Input shape must be [frames, height, width, 3]."
 
         def input_iterator():
             # return windows of size 100 where the first/last 25 frames are from the previous/next batch
             # the first and last window must be padded by copies of the first and last frame of the video
             no_padded_frames_start = 25
-            no_padded_frames_end = 25 + 50 - (len(frames) % 50 if len(frames) % 50 != 0 else 50)  # 25 - 74
+            no_padded_frames_end = (
+                25 + 50 - (len(frames) % 50 if len(frames) % 50 != 0 else 50)
+            )  # 25 - 74
 
             start_frame = np.expand_dims(frames[0], 0)
             end_frame = np.expand_dims(frames[-1], 0)
             padded_inputs = np.concatenate(
-                [start_frame] * no_padded_frames_start + [frames] + [end_frame] * no_padded_frames_end, 0
+                [start_frame] * no_padded_frames_start
+                + [frames]
+                + [end_frame] * no_padded_frames_end,
+                0,
             )
 
             ptr = 0
             while ptr + 100 <= len(padded_inputs):
-                out = padded_inputs[ptr:ptr + 100]
+                out = padded_inputs[ptr : ptr + 100]
                 ptr += 50
                 yield out[np.newaxis]
 
@@ -58,33 +68,54 @@ class TransNetV2:
 
         for inp in input_iterator():
             single_frame_pred, all_frames_pred = self.predict_raw(inp)
-            predictions.append((single_frame_pred.numpy()[0, 25:75, 0],
-                                all_frames_pred.numpy()[0, 25:75, 0]))
+            predictions.append(
+                (
+                    single_frame_pred.numpy()[0, 25:75, 0],
+                    all_frames_pred.numpy()[0, 25:75, 0],
+                )
+            )
 
-            print("\r[TransNetV2] Processing video frames {}/{}".format(
-                min(len(predictions) * 50, len(frames)), len(frames)
-            ), end="")
+            print(
+                "\r[TransNetV2] Processing video frames {}/{}".format(
+                    min(len(predictions) * 50, len(frames)), len(frames)
+                ),
+                end="",
+            )
         print("")
 
         single_frame_pred = np.concatenate([single_ for single_, all_ in predictions])
         all_frames_pred = np.concatenate([all_ for single_, all_ in predictions])
 
-        return single_frame_pred[:len(frames)], all_frames_pred[:len(frames)]  # remove extra padded frames
+        return (
+            single_frame_pred[: len(frames)],
+            all_frames_pred[: len(frames)],
+        )  # remove extra padded frames
 
     def predict_video(self, video_fn: str):
         try:
             import ffmpeg
         except ModuleNotFoundError:
-            raise ModuleNotFoundError("For `predict_video` function `ffmpeg` needs to be installed in order to extract "
-                                      "individual frames from video file. Install `ffmpeg` command line tool and then "
-                                      "install python wrapper by `pip install ffmpeg-python`.")
+            raise ModuleNotFoundError(
+                "For `predict_video` function `ffmpeg` needs to be installed in order to extract "
+                "individual frames from video file. Install `ffmpeg` command line tool and then "
+                "install python wrapper by `pip install ffmpeg-python`."
+            )
 
         print("[TransNetV2] Extracting frames from {}".format(video_fn))
-        video_stream, err = ffmpeg.input(video_fn).output(
-            "pipe:", format="rawvideo", pix_fmt="rgb24", s="48x27"
-        ).run(capture_stdout=True, capture_stderr=True)
+        video_stream, err = (
+            ffmpeg.input(video_fn)
+            .output(
+                "pipe:",
+                format="rawvideo",
+                pix_fmt="rgb24",
+                s="{}x{}".format(self._input_size[1], self._input_size[0]),
+            )
+            .run(capture_stdout=True, capture_stderr=True)
+        )
 
-        video = np.frombuffer(video_stream, np.uint8).reshape([-1, 27, 48, 3])
+        video = np.frombuffer(video_stream, np.uint8).reshape(
+            [-1, self._input_size[0], self._input_size[1], 3]
+        )
         return (video, *self.predict_frames(video))
 
     @staticmethod
@@ -127,9 +158,9 @@ class TransNetV2:
         height = len(frames) // width
 
         img = frames.reshape([height, width, ih + 1, iw + len(predictions), ic])
-        img = np.concatenate(np.split(
-            np.concatenate(np.split(img, height), axis=2)[0], width
-        ), axis=2)[0, :-1]
+        img = np.concatenate(
+            np.split(np.concatenate(np.split(img, height), axis=2)[0], width), axis=2
+        )[0, :-1]
 
         img = Image.fromarray(img)
         draw = ImageDraw.Draw(img)
@@ -155,22 +186,39 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("files", type=str, nargs="+", help="path to video files to process")
-    parser.add_argument("--weights", type=str, default=None,
-                        help="path to TransNet V2 weights, tries to infer the location if not specified")
-    parser.add_argument('--visualize', action="store_true",
-                        help="save a png file with prediction visualization for each extracted video")
+    parser.add_argument(
+        "files", type=str, nargs="+", help="path to video files to process"
+    )
+    parser.add_argument(
+        "--weights",
+        type=str,
+        default=None,
+        help="path to TransNet V2 weights, tries to infer the location if not specified",
+    )
+    parser.add_argument(
+        "--visualize",
+        action="store_true",
+        help="save a png file with prediction visualization for each extracted video",
+    )
     args = parser.parse_args()
 
     model = TransNetV2(args.weights)
     for file in args.files:
-        if os.path.exists(file + ".predictions.txt") or os.path.exists(file + ".scenes.txt"):
-            print(f"[TransNetV2] {file}.predictions.txt or {file}.scenes.txt already exists. "
-                  f"Skipping video {file}.", file=sys.stderr)
+        if os.path.exists(file + ".predictions.txt") or os.path.exists(
+            file + ".scenes.txt"
+        ):
+            print(
+                f"[TransNetV2] {file}.predictions.txt or {file}.scenes.txt already exists. "
+                f"Skipping video {file}.",
+                file=sys.stderr,
+            )
             continue
 
-        video_frames, single_frame_predictions, all_frame_predictions = \
-            model.predict_video(file)
+        (
+            video_frames,
+            single_frame_predictions,
+            all_frame_predictions,
+        ) = model.predict_video(file)
 
         predictions = np.stack([single_frame_predictions, all_frame_predictions], 1)
         np.savetxt(file + ".predictions.txt", predictions, fmt="%.6f")
@@ -180,12 +228,17 @@ def main():
 
         if args.visualize:
             if os.path.exists(file + ".vis.png"):
-                print(f"[TransNetV2] {file}.vis.png already exists. "
-                      f"Skipping visualization of video {file}.", file=sys.stderr)
+                print(
+                    f"[TransNetV2] {file}.vis.png already exists. "
+                    f"Skipping visualization of video {file}.",
+                    file=sys.stderr,
+                )
                 continue
 
             pil_image = model.visualize_predictions(
-                video_frames, predictions=(single_frame_predictions, all_frame_predictions))
+                video_frames,
+                predictions=(single_frame_predictions, all_frame_predictions),
+            )
             pil_image.save(file + ".vis.png")
 
 
